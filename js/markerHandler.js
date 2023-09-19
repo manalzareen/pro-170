b1b7c553c45546a2c7bb258b93d71b3b955bb106
manalzareen <81857933+manalzareen@users.noreply.github.com>
2023-09-19 17:24:53 +0530

    Add files via upload

---
+++ js/markerHandler.js
@@ -0,0 +1,50 @@
+AFRAME.registerComponent("markerhandler", {
+    init: async function () {
+         this.el.addEventListener("markerFound", () => {
+              //console.log("marker is found")
+              this.handleMarkerFound();
+         })
+         this.el.addEventListener("markerLost", () => {
+              //console.log("marker is lost")
+              this.handleMarkerLost();
+
+         })
+
+    },
+    handleMarkerFound: function (dishes, markerId) {
+        // Changing button div visibility
+        var buttonDiv = document.getElementById("button-div");
+        buttonDiv.style.display = "flex";
+    
+        var orderSummaryButton = document.getElementById("order-summary-button");
+        var orderButtton = document.getElementById("order-button");
+    
+        // Handling Click Events
+        orderSummaryButton.addEventListener("click", function () {
+          swal({
+            icon: "warning",
+            title: "Order Summary",
+            text: "Work In Progress"
+          });
+        });
+    
+        orderButtton.addEventListener("click", () => {
+          swal({
+            icon: "https://i.imgur.com/4NZ6uLY.jpg",
+            title: "Thanks For Order !",
+            text: "Your order will serve soon on your table!"
+          });
+        });
+    
+       
+      },
+    
+      handleMarkerLost: function () {
+        // Changing button div visibility
+        var buttonDiv = document.getElementById("button-div");
+        buttonDiv.style.display = "none";
+      },
+      
+    
+
+});
